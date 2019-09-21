2de1790972f1cf876220ac7e4f3dc68c01e12643
TheG-Man <36961383+TheG-Man@users.noreply.github.com>
2019-09-21 19:04:48 +0300

    Add lab-1

---
+++ lab-1/1.sql
@@ -0,0 +1,19 @@
+﻿CREATE DATABASE [Vadim_Seledets_DB];
+GO
+
+CREATE SCHEMA [sales];
+GO
+
+CREATE SCHEMA [persons];
+GO
+
+CREATE TABLE [sales].[Orders] (OrderNum INT NULL);
+
+BACKUP DATABASE [Vadim_Seledets_DB] TO DISK = 'C:\Users\nikol\Desktop\Databases\Vadim_Seledets_DB.bak';
+GO
+
+DROP DATABASE [Vadim_Seledets_DB];
+GO
+
+RESTORE DATABASE [Vadim_Seledets_DB] FROM DISK = 'C:\Users\nikol\Documents\SQL Server Management Studio\Vadim_Seledets_DB.bak';
+GO
---
+++ lab-1/V3_2.sql
@@ -0,0 +1,29 @@
+-- Task 1
+
+SELECT [DepartmentID], [Name] FROM [AdventureWorks2012].[HumanResources].[Department]
+WHERE LEFT(Name, 1) = 'P'
+
+-- Task 2
+
+SELECT 
+	[BusinessEntityID],
+	[JobTitle],
+	[Gender],
+	[VacationHours],
+	[SickLeaveHours]
+FROM [AdventureWorks2012].[HumanResources].[Employee]
+WHERE VacationHours BETWEEN 10 AND 13
+
+-- Task 3
+
+SELECT 
+	[BusinessEntityID],
+	[JobTitle],
+	[Gender],
+	[BirthDate],
+	[HireDate]
+FROM [AdventureWorks2012].[HumanResources].[Employee]
+WHERE MONTH(HireDate) = 7 AND DAY(HireDate) = 1
+ORDER BY [BusinessEntityID] ASC
+	OFFSET 3 ROWS
+	FETCH NEXT 5 ROWS ONLY
